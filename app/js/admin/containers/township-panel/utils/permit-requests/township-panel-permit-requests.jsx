7dc2e71f0e0dd4cd4a9dece846d55d62e479b4c2
troyedwardsjr <troyedwardsjr@gmail.com>
2016-06-29 14:41:32 -0400

    Permit Requests List fixed.

--- app/js/admin/containers/township-panel/utils/permit-requests/township-panel-permit-requests.jsx
+++ app/js/admin/containers/township-panel/utils/permit-requests/township-panel-permit-requests.jsx
@@ -14,7 +14,7 @@ import Griddle from 'griddle-react'
 import { BootstrapPager, GriddleBootstrap } from 'griddle-react-bootstrap'
 import {customFilterComponent, customFilterFunction} from '../../../../common/components/griddle-custom-filter.jsx'
 
-import {createFilter} from 'react-search-input';
+import _ from "lodash";
 
 var classNames = {
   content: 'traditional-tabs-content',
@@ -53,30 +53,38 @@ class customColumnComponent extends React.Component {
   }
 
   render() {
-    console.log(this.props.rowData)
-    if(this.props.data === "YES") {
-      return (
-        <div style={{color: "blue"}}>
-          <i className="material-icons valign" style={{color: "#2DB63D"}}>check_box</i>
-        </div>
-      );
-    } else if (this.props.data === "NO") {
-      return (
-        <div style={{color: "blue"}}>
-          <i className="material-icons valign" style={{color: "#EAB41C"}}>new_releases</i>
-        </div>
-      );
+
+    if(this.props.metadata.customComponentMetadata.approveField) {
+      if(this.props.data === "YES" && this.props.rowData.status === "ACTIVE") {
+        return (
+          <div style={{color: "blue"}} onClick={() => this.props.metadata.customComponentMetadata.renderEditModal(this.props.rowData.id)}>
+            <i className="material-icons valign" style={{color: "#2DB63D"}}>check_box</i>
+          </div>
+        );
+      } else if (this.props.data === "NO" && this.props.rowData.status === "INACTIVE") {
+        return (
+          <div style={{color: "blue"}} onClick={() => this.props.metadata.customComponentMetadata.renderEditModal(this.props.rowData.id)}>
+            <i className="material-icons valign" style={{color: "#EAB41C"}}>new_releases</i>
+          </div>
+        );
+      } else if (this.props.data === "NO"  && this.props.rowData.status === "ACTIVE") {
+        return (
+          <div style={{color: "blue"}} onClick={() => this.props.metadata.customComponentMetadata.renderEditModal(this.props.rowData.id)}>
+            <i className="material-icons valign" style={{color: "#D9534F"}}>indeterminate_check_box</i>
+          </div>
+        );
+      }
     } else {
       return (
-        <div style={{color: "blue"}}>
-          <i className="material-icons valign" style={{color: "#D9534F"}}>indeterminate_check_box</i>
+        <div onClick={() => this.props.metadata.customComponentMetadata.renderEditModal(this.props.rowData.id)}>
+          {this.props.data}
         </div>
       );
     }
   }
 }
 
-customColumnComponent.defaultProps = { "data": {}, "renderEditModal": null};
+customColumnComponent.defaultProps = { "data": {}, "renderEditModal": null, "approvedField": false};
 
 export default class TownshipPanelPermitRequests extends React.Component {
 
@@ -93,7 +101,7 @@ export default class TownshipPanelPermitRequests extends React.Component {
       showEditModal: false,
       rowData: null,
       selectizeOptions: {},
-      requestName: 'Name Unavailable'
+      requestId: 'Name Unavailable'
     }
 
     this.renderApproveModal = this.renderApproveModal.bind(this);
@@ -119,12 +127,11 @@ export default class TownshipPanelPermitRequests extends React.Component {
   }
 
   renderApproveModal() {
-    
     return(
      <div id="permit-requests-modal" className="modal">
       <div className="modal-content">
         <h4>Approve or Deny Permit Request </h4>
-        <p>Request ID: {this.state.requestName}</p>
+        <p>Request ID: {this.state.requestId}</p>
       </div>
       <div className="modal-footer">
         <div className="row marginless-row">
@@ -133,12 +140,12 @@ export default class TownshipPanelPermitRequests extends React.Component {
           </div>
           <div className="col s3">
             <a className="waves-effect waves-light btn btn-red" onClick={
-              () => this.props.editTownshipPermitRequests({"approved": "NO", "status": "ACTIVE"}, this.state.requestName)
+              () => this.props.editTownshipPermitRequests({"approved": "NO", "status": "ACTIVE"}, this.state.requestId)
             }>Reject</a>
           </div>
           <div className="col s3">
             <a className="waves-effect waves-light btn btn-green" onClick={
-              () => this.props.editTownshipPermitRequests({"approved": "YES", "status": "ACTIVE"}, this.state.requestName)
+              () => this.props.editTownshipPermitRequests({"approved": "YES", "status": "ACTIVE"}, this.state.requestId)
             }>Approve</a>
           </div>
         </div>
@@ -147,75 +154,64 @@ export default class TownshipPanelPermitRequests extends React.Component {
     );
   }
 
-  renderPendingData(filteredSubscriptions) {
-    if(filteredSubscriptions.resource.length > 0){   
-      return filteredSubscriptions.resource.map((request) => {
-        if(request.status === 'INACTIVE') {
-          return( 
-          <tbody>
-            <tr key={request.id} onClick={() => { 
-                this.setState({requestName: request.id});
-                $('#permit-requests-modal').openModal();
-              }
-            }>
-              <td>{request.id}</td>
-              <td>{request.township_code}</td>
-              <td>{request.township_name}</td>
-              <td>{request.permit_type}</td>
-              <td>{request.permit_name}</td>
-              <td>{request.residency_proof}</td>
-
-              <td><i className="material-icons valign" style={{color: "#EAB41C"}}>new_releases</i> </td>
-
-              <td>{request.date_action}</td>
-              <td>{request.status}</td>
-              <td>{request.paid}</td>
-              <td>{request.user_comments}</td>
-              <td>{request.town_comments}</td>
-              <td>{request.logo}</td>
-              <td>{request.schema_type}</td>
-              <td>{request.first_contact_date}</td>
-              <td>{request.rate}</td>
-              <td>{request.user_name}</td>
-            </tr>
-          </tbody>
-          );
-        }
-      });
-    } else {
-      return( 
-        <tr>
-          <td>There are no pending requests.</td> 
-        </tr>
-      );
-    }
-  }
-
-  renderEditModal(recordId, rowData) {
-    console.log(rowData);
-    console.log(recordId);
-    window.scrollTo(0, document.body.scrollHeight);
-    this.setState({showEditDuplicateButtons: true, rowData: rowData, showEditModal: true, parkingLocationCode: recordId})
+  renderEditModal(requestId) {
+    this.setState({requestId: requestId});
+    $('#permit-requests-modal').openModal();
   }
 
   renderPendingTable() {
 
    var filteredSubscriptions = this.props.townshipPermitRequestsFetched.data;
-   console.log(filteredSubscriptions.resource);
 
     var columnMeta = [
       {
         "columnName": "approved",
         "customComponent": customColumnComponent
-      }
+      },
+      {
+        "columnName": "permit_type",
+        "customComponent": customColumnComponent
+      },
     ];
+
+    var renderEditModal = this.renderEditModal;
+    var metaDataFunction = () =>  {
+      return fields.map((data) => {
+        if(data === "approved") {
+          return (
+            {
+              "columnName": "approved",
+              "customComponent": customColumnComponent,
+              'customComponentMetadata': {
+                'approveField': true,
+                'renderEditModal': renderEditModal
+              }
+            }
+          );
+        } else {
+          return (
+            {
+              "columnName": data,
+              "customComponent": customColumnComponent,
+              'customComponentMetadata': {
+                'approveField': false,
+                'renderEditModal': renderEditModal
+              }
+            }
+          );
+        }
+      });
+    }
+
+    const filteredRequests = _.filter(filteredSubscriptions.resource, {'approved': 'NO', status: 'INACTIVE'});
+    console.log(filteredRequests)
 
     return(
       <Griddle
         tableClassName={'table table-bordered table-striped table-hover'}
         filterClassName={''}
         useGriddleStyles={false}
-        results={filteredSubscriptions.resource}
+        results={filteredRequests}
         showFilter={true}
         showSettings={true}
         settingsToggleClassName='btn btn-default'
@@ -225,7 +221,76 @@ export default class TownshipPanelPermitRequests extends React.Component {
         customFilterComponent={customFilterComponent}
         useCustomFilterer={true} 
         customFilterer={customFilterFunction}
-        columnMetadata={columnMeta}
+        columnMetadata={metaDataFunction()}
+        columns={['id',  
+                  'date_time', 
+                  'user_id', 
+                  'township_code', 
+                  'township_name', 
+                  'permit_type', 
+                  'permit_name', 
+                  'residency_proof', 
+                  'approved',  ]}
+      />
+    );
+  }
+  renderApprovedTable() {
+   var filteredSubscriptions = this.props.townshipPermitRequestsFetched.data;
+
+   var columnMeta = [
+      {
+        "columnName": "approved",
+        "customComponent": customColumnComponent
+      }
+    ];
+
+    const filteredRequests = _.filter(filteredSubscriptions.resource, { 'approved': 'YES'});
+
+    var renderEditModal = this.renderEditModal;
+    var metaDataFunction = () =>  {
+      return fields.map((data) => {
+        if(data === "approved") {
+          return (
+            {
+              "columnName": "approved",
+              "customComponent": customColumnComponent,
+              'customComponentMetadata': {
+                'approveField': true,
+                'renderEditModal': renderEditModal
+              }
+            }
+          );
+        } else {
+          return (
+            {
+              "columnName": data,
+              "customComponent": customColumnComponent,
+              'customComponentMetadata': {
+                'approveField': false,
+                'renderEditModal': renderEditModal
+              }
+            }
+          );
+        }
+      });
+    }
+
+    return(
+      <Griddle
+        tableClassName={'table table-bordered table-striped table-hover'}
+        filterClassName={''}
+        useGriddleStyles={false}
+        results={filteredRequests}
+        showFilter={true}
+        showSettings={true}
+        settingsToggleClassName='btn btn-default'
+        useCustomPagerComponent={true}
+        customPagerComponent={ BootstrapPager }
+        useCustomFilterComponent={true} 
+        customFilterComponent={customFilterComponent}
+        useCustomFilterer={true} 
+        customFilterer={customFilterFunction}
+        columnMetadata={metaDataFunction()}
         columns={['id',  
                   'date_time', 
                   'user_id', 
@@ -239,172 +304,76 @@ export default class TownshipPanelPermitRequests extends React.Component {
     );
   }
 
-  renderApprovedData(filteredSubscriptions) {
-    if(filteredSubscriptions.resource.length > 0){   
-      return filteredSubscriptions.resource.map((request) => {
-        if(request.approved === 'YES' && request.status === 'ACTIVE') {
-          return( 
-            <tbody>
-              <tr key={request.id} onClick={() => { 
-                  this.setState({requestName: request.id});
-                  $('#permit-requests-modal').openModal();
-                }
-              }>
-                <td>{request.id}</td>
-                <td>{request.township_code}</td>
-                <td>{request.township_name}</td>
-                <td>{request.permit_type}</td>
-                <td>{request.permit_name}</td>
-                <td>{request.residency_proof}</td>
-
-                <td><i className="material-icons valign" style={{color: "#2DB63D"}}>check_box</i></td>
-
-                <td>{request.date_action}</td>
-                <td>{request.status}</td>
-                <td>{request.paid}</td>
-                <td>{request.user_comments}</td>
-                <td>{request.town_comments}</td>
-                <td>{request.logo}</td>
-                <td>{request.schema_type}</td>
-                <td>{request.first_contact_date}</td>
-                <td>{request.rate}</td>
-                <td>{request.user_name}</td>
-              </tr>
-          </tbody>
-          );
-        }
-      });
-    } else {
-      return( 
-        <tr>
-          <td>There are no pending requests.</td> 
-        </tr>
-      );
-    }
-  }
-
-  renderApprovedTable() {
-    /*
-    if(this.props.townshipPermitRequestsFetched.isLoading === false) {
-      console.log(this.props.townshipPermitRequestsFetched);
-    }
-    */
-   var filteredSubscriptions = this.props.townshipPermitRequestsFetched.data;
-   console.log(filteredSubscriptions.resource);
-    return(
-      <div className="township-permitrequests-container">
-        <table className="highlight">
-          <thead>
-            <tr>
-              <th data-field="id">ID</th>
-              <th data-field="name">Township Code</th>
-              <th data-field="price">Township Name</th>
-              <th data-field="price">Permit Type</th>
-              <th data-field="price">Permit Name</th>
-              <th data-field="price">Residency Proof</th>
-              <th data-field="price">Approved</th>
-              <th data-field="price">Date Action</th>
-              <th data-field="price">Status</th>
-              <th data-field="price">Paid</th>
-              <th data-field="price">User Comments</th>
-              <th data-field="price">Town Comments</th>
-              <th data-field="price">Logo</th>
-              <th data-field="price">Scheme Type</th>
-              <th data-field="price">First Contact Date</th>
-              <th data-field="price">Rate</th>
-              <th data-field="price">User Name</th>
-            </tr>
-          </thead>
-          {this.renderApprovedData(filteredSubscriptions)}
-        </table>
-      </div>
-    );
-  }
-
-  renderRejectedData(filteredSubscriptions) {
-    if(filteredSubscriptions.resource.length > 0){   
-      return filteredSubscriptions.resource.map((request) => {
-        if(request.approved === 'NO' && request.status === 'ACTIVE') {
-          return( 
-          <tbody>
-            <tr key={request.id} onClick={() => { 
-                this.setState({requestName: request.id});
-                $('#permit-requests-modal').openModal();
-              }
-            }>
-              <td>{request.id}</td>
-              <td>{request.township_code}</td>
-              <td>{request.township_name}</td>
-              <td>{request.permit_type}</td>
-              <td>{request.permit_name}</td>
-              <td>{request.residency_proof}</td>
-
-              <td><i className="material-icons valign" style={{color: "#D9534F"}}>indeterminate_check_box</i> </td>
-
-              <td>{request.date_action}</td>
-              <td>{request.status}</td>
-              <td>{request.paid}</td>
-              <td>{request.user_comments}</td>
-              <td>{request.town_comments}</td>
-              <td>{request.logo}</td>
-              <td>{request.schema_type}</td>
-              <td>{request.first_contact_date}</td>
-              <td>{request.rate}</td>
-              <td>{request.user_name}</td>
-            </tr>
-          </tbody>
-          );
-        }
-      });
-    } else {
-      return( 
-        <tr>
-          <td>There are no pending requests.</td> 
-        </tr>
-      );
-    }
-  }
-
   renderRejectedTable() {
-    /*
-    if(this.props.townshipPermitRequestsFetched.isLoading === false) {
-      console.log(this.props.townshipPermitRequestsFetched);
-    }
-    */
    var filteredSubscriptions = this.props.townshipPermitRequestsFetched.data;
-   console.log(filteredSubscriptions.resource);
+    var columnMeta = [
+      {
+        "columnName": "approved",
+        "customComponent": customColumnComponent
+      }
+    ];
+
+    const filteredRequests = _.filter(filteredSubscriptions.resource, { 'approved': 'NO', status: 'ACTIVE'});
+
+    var renderEditModal = this.renderEditModal;
+    var metaDataFunction = () =>  {
+      return fields.map((data) => {
+        if(data === "approved") {
+          return (
+            {
+              "columnName": "approved",
+              "customComponent": customColumnComponent,
+              'customComponentMetadata': {
+                'approveField': true,
+                'renderEditModal': renderEditModal
+              }
+            }
+          );
+        } else {
+          return (
+            {
+              "columnName": data,
+              "customComponent": customColumnComponent,
+              'customComponentMetadata': {
+                'approveField': false,
+                'renderEditModal': renderEditModal
+              }
+            }
+          );
+        }
+      });
+    }
+
     return(
-      <div className="township-permitrequests-container">
-        <table className="highlight">
-          <thead>
-            <tr>
-              <th data-field="id">ID</th>
-              <th data-field="name">Township Code</th>
-              <th data-field="price">Township Name</th>
-              <th data-field="price">Permit Type</th>
-              <th data-field="price">Permit Name</th>
-              <th data-field="price">Residency Proof</th>
-              <th data-field="price">Approved</th>
-              <th data-field="price">Date Action</th>
-              <th data-field="price">Status</th>
-              <th data-field="price">Paid</th>
-              <th data-field="price">User Comments</th>
-              <th data-field="price">Town Comments</th>
-              <th data-field="price">Logo</th>
-              <th data-field="price">Scheme Type</th>
-              <th data-field="price">First Contact Date</th>
-              <th data-field="price">Rate</th>
-              <th data-field="price">User Name</th>
-            </tr>
-          </thead>
-          {this.renderRejectedData(filteredSubscriptions)}
-        </table>
-      </div>
+      <Griddle
+        tableClassName={'table table-bordered table-striped table-hover'}
+        filterClassName={''}
+        useGriddleStyles={false}
+        results={filteredRequests}
+        showFilter={true}
+        showSettings={true}
+        settingsToggleClassName='btn btn-default'
+        useCustomPagerComponent={true}
+        customPagerComponent={ BootstrapPager }
+        useCustomFilterComponent={true} 
+        customFilterComponent={customFilterComponent}
+        useCustomFilterer={true} 
+        customFilterer={customFilterFunction}
+        columnMetadata={metaDataFunction()}
+        columns={['id',  
+                  'date_time', 
+                  'user_id', 
+                  'township_code', 
+                  'township_name', 
+                  'permit_type', 
+                  'permit_name', 
+                  'residency_proof', 
+                  'approved',  ]}
+      />
     );
   }
 
   render() {
-    console.log(this.props.townshipPermitRequestsEdited)
     return (
       <div className="blue-body marginless-row">
         <Body showHeader={true}>
